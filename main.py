9db3a9d02bbfcd6ffc43255c0c9b70432c01f9ee
Leo Smith <ecomstoreleo@gmail.com>
2024-10-12 19:57:00 +0200

    add color retrieval and sorting

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-import flask, numpy as np
+import numpy as np
 from PIL import Image
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 
-
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/', methods=["POST", "GET"])
 def extract():
     if request.method == 'POST':
-        file = request.files['image']
+        file = request.files.get('image')
+        print(file)
 
         if file:
             img = Image.open(file)
             img = img.resize((100, 100))
+
+            img = img.convert('RGB')
+
             img_array = np.array(img)
 
-            #process
+            pixels = img_array.reshape(-1, 3)
 
-            return render_template('index.html', colors=img_array)  #
+            unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
+
+            color_counts = list(zip(counts, unique_colors.tolist()))
+
+            color_counts.sort(reverse=True, key=lambda x: x[0])
+
+            print("Most Common Colors:")
+            for count, color in color_counts[:10]:
+                print(f"Color: {color}, Count: {count}")
+
+            return render_template('index.html', colors=color_counts[:10])
 
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
