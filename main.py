853be6589f8f673baa5bce7f7093f4f9f4691f3e
Leo Smith <ecomstoreleo@gmail.com>
2024-11-26 20:19:35 +0200

    add error catching, display color block and corresponding information

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 import numpy as np
 from PIL import Image
-from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.secret_key = 'your_secret_key'  # Set a secret key for flash messages
 
 @app.route('/', methods=["POST", "GET"])
 def extract():
     if request.method == 'POST':
         file = request.files.get('image')
-        print(file)
-
         if file:
-            img = Image.open(file)
-            img = img.resize((100, 100))
-
-            img = img.convert('RGB')
-
-            img_array = np.array(img)
-
-            pixels = img_array.reshape(-1, 3)
-
-            unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
-
-            color_counts = list(zip(counts, unique_colors.tolist()))
-
-            color_counts.sort(reverse=True, key=lambda x: x[0])
-
-            print("Most Common Colors:")
-            for count, color in color_counts[:10]:
-                print(f"Color: {color}, Count: {count}")
-
-            return render_template('index.html', colors=color_counts[:10])
-
+            try:
+                img = Image.open(file)
+                img = img.resize((100, 100))
+                img = img.convert('RGB')
+                img_array = np.array(img)
+                pixels = img_array.reshape(-1, 3)
+                unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
+                color_counts = list(zip(counts, unique_colors.tolist()))
+                color_counts.sort(reverse=True, key=lambda x: x[0])
+                flash('File uploaded successfully!', 'success')
+                return render_template('index.html', colors=color_counts[:10])
+            except:
+                flash('Error processing the image. Please try again.', 'danger')
+                return render_template('index.html')
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
